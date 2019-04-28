e15e2fe7699af5e00182d698ba0751673ead14b7
zxc <44724385@qq.com>
2019-04-28 19:59:34 +0800

    add game page entry

---
+++ slimvue-succotash-company-homepage/succotash-company-homepage/components/game/UnderworldLife.vue
@@ -0,0 +1,31 @@
+<template>
+    <GamePageTpl
+        :screenShotImages="this.screenShotImages"
+        :slideImages="this.slideImages"
+        :gameDescription="this.gameDescription"
+    ></GamePageTpl>
+</template>
+
+<script>
+    import GamePageTpl from '../sub/GamePageTpl';
+    export default {
+        name: "underworld-life",
+        components : {
+            GamePageTpl,
+        },
+        data : function () {
+            return {
+                screenShotImages : {
+                    img1: require("assets/images/hb_pic1.jpg"),
+                    img2: require("assets/images/hb_pic2.jpg"),
+                    img3: require("assets/images/hb_pic3.jpg"),
+                    img4: require("assets/images/hb_pic4.jpg"),
+                },
+                slideImages : {
+                    img2: require("assets/images/top_bg2.jpg"),
+                },
+                gameDescription: "\"Underworld life\" is a global RPG mobile game masterpiece. In the game, you will become the protagonist, lead the Capos to revenge for the father and reinvigorate the family glory. The real scene restoration of underworld includes capo, beauty, smuggling, kidnapping, casino, and firefight systems. The battle for becoming godfather has been unveiled. The godfather's legendary life awaits your participation."
+            };
+        }
+    };
+</script>
--- slimvue-succotash-company-homepage/succotash-company-homepage/components/sub/GamePageTpl.vue
+++ slimvue-succotash-company-homepage/succotash-company-homepage/components/sub/GamePageTpl.vue
@@ -13,7 +13,6 @@
             </div>
             <div id="game-description-block">
                 <p>{{gameDescription}}</p>
-
             </div>
             <div class="screen-shots-title">
                 <span>SCREENS</span>
@@ -40,20 +39,19 @@
             topMenu,
             ImageSwiper
         },
-        data : function () {
-            return {
-                imageItemClass : 'screen-shot-item',
-                screenShotImages : {
-                    img1: require("assets/images/hb_pic1.jpg"),
-                    img2: require("assets/images/hb_pic2.jpg"),
-                    img3: require("assets/images/hb_pic3.jpg"),
-                    img4: require("assets/images/hb_pic4.jpg"),
-                },
-                slideImages : {
-                    img2: require("assets/images/top_bg2.jpg"),
-                },
-                gameDescription: "\"Underworld life\" is a global RPG mobile game masterpiece. In the game, you will become the protagonist, lead the Capos to revenge for the father and reinvigorate the family glory. The real scene restoration of underworld includes capo, beauty, smuggling, kidnapping, casino, and firefight systems. The battle for becoming godfather has been unveiled. The godfather's legendary life awaits your participation."
-            };
+        props : {
+            screenShotImages: {
+                type     : Object,
+                required : true,
+            },
+            slideImages: {
+                type     : Object,
+                required : true,
+            },
+            gameDescription: {
+                type     : String,
+                required : true,
+            },
         }
     };
 </script>
